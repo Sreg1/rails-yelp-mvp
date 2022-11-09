b22db12803daabfef1da842e74bcda0e121fbf31
Anabelle Rogers <asregor@hotmail.com>
2022-11-09 11:49:55 -0800

    added validations in restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependant: :destroy
+
+  CATEGORIES = %w[chinese, italian, japanese, french, belgian]
+
+  validates :name, presence: true
+  validates :categories, inclusion: { in: CATEGORIES }
 end
