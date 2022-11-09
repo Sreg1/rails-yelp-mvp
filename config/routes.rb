f8ae4fac0fe0177e756f3eede4ef9c9f8689872e
Anabelle Rogers <asregor@hotmail.com>
2022-11-09 11:39:42 -0800

    2 models created + routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # LIST RESTAURANTS
+  GET 'restaurants', to: 'restaurants#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # ADD RESTAURANT (redirects to the show view of that new restaurant)
+  # return HTML form to create the new restaurant
+  GET 'restaurants/new', to: 'restaurants#new'
+  # creates a new restaurant
+  POST 'restaurants', to: 'restaurants#create'
+
+  # DETAILS RESTAURANT (including reviews)
+  GET 'restaurants/:id', to: 'restaurants#show'
+
+  # ADD REVIEW to a restaurant
+  # return HTML form to edit the review
+  GET 'restaurants/:id/reviews/new', to: 'restaurants/reviews#new'
+  # creates a new review
+  POST 'restaurants/:id/reviews', to: 'restaurants/reviews#create'
 end
